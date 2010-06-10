4ef25e06e7e50568bd7c9eb089f63b3b78b150d2
Rufo Sanchez <rufo@rufosanchez.com>
2010-06-10 12:30:57 -0400

    added keyboard shortcut preference handling code

--- Instafari.safariextension/hotkey_script.js
+++ Instafari.safariextension/hotkey_script.js
@@ -1,7 +1,36 @@
+var keyboard_shortcut;
+
+function getSettings(event) {
+  if (event.name === "keyboard_shortcut") {
+    keyboard_shortcut = event.message;
+  }
+}
+
 document.addEventListener('keydown', function(event){
-  if (event.metaKey && event.altKey && (event.keyCode == 82)) {
-    safari.self.tab.dispatchMessage("readlater");
-  } else {
-    return false;
+  var sendMessage = false;
+  
+  var tag = event.target.tagName;
+  if (tag === 'TEXTAREA' || tag === 'INPUT' || tag === 'SELECT') {
+    return;
+  }
+    
+  if (keyboard_shortcut === "mac") {
+    if (event.metaKey && event.altKey && (event.keyCode == 82)) {
+      sendMessage = true;
+    }
+  } else if (keyboard_shortcut === "windows") {
+    if (event.ctrlKey && event.altKey && (event.keyCode == 82)) {
+      sendMessage = true;
+    }
+  }
+  
+  if (sendMessage) {
+     safari.self.tab.dispatchMessage("readlater");
+     event.stopPropagation();
+     event.preventDefault();
+     return true;
   }
 }, false);
+
+safari.self.addEventListener("message", getSettings, false);
+safari.self.tab.dispatchMessage("getSettings", "keyboard_shortcut");
