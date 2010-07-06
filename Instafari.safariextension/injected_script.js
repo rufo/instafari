86cb2099fdaca59252a61735fe10c2503e03ac52
Rufo Sanchez <rufo@rufosanchez.com>
2010-07-06 15:40:41 -0400

    added contextual menu, fixed problems with tabs and multiple submission to Instapaper

--- Instafari.safariextension/injected_script.js
+++ Instafari.safariextension/injected_script.js
@@ -1,38 +1,50 @@
-var keyboard_shortcut;
+if(window === window.top) {
+  var keyboard_shortcut;
 
-function messageHandler(event) {
-  if (event.name === "keyboard_shortcut") {
-    keyboard_shortcut = event.message;
-  } else if (event.name === "get_selection") {
-    safari.self.tab.dispatchMessage("returnSelection", window.getSelection().toString());
+  function messageHandler(event) {
+    var eventHandled;
+
+    if (event.name === "keyboard_shortcut") {
+      keyboard_shortcut = event.message;
+      eventHandled = true;
+    } else if (event.name === "get_selection") {
+      safari.self.tab.dispatchMessage("returnSelection", window.getSelection().toString());
+      eventHandled = true;
+    }
+
+    if (eventHandled){
+      event.stopPropagation();
+      event.preventDefault();
+      return true;
+    }
   }
+
+  document.addEventListener('keydown', function(event){
+    var sendMessage = false;
+
+    var tag = event.target.tagName;
+    if (tag === 'TEXTAREA' || tag === 'INPUT' || tag === 'SELECT') {
+      return;
+    }
+
+    if (keyboard_shortcut === "mac") {
+      if (event.metaKey && event.altKey && (event.keyCode == 82)) {
+        sendMessage = true;
+      }
+    } else if (keyboard_shortcut === "windows") {
+      if (event.ctrlKey && event.altKey && (event.keyCode == 82)) {
+        sendMessage = true;
+      }
+    }
+
+    if (sendMessage) {
+       safari.self.tab.dispatchMessage("readlater");
+       event.stopPropagation();
+       event.preventDefault();
+       return true;
+    }
+  }, false);
+
+  safari.self.addEventListener("message", messageHandler, false);
+  safari.self.tab.dispatchMessage("getSettings", "keyboard_shortcut");
 }
-
-document.addEventListener('keydown', function(event){
-  var sendMessage = false;
-  
-  var tag = event.target.tagName;
-  if (tag === 'TEXTAREA' || tag === 'INPUT' || tag === 'SELECT') {
-    return;
-  }
-    
-  if (keyboard_shortcut === "mac") {
-    if (event.metaKey && event.altKey && (event.keyCode == 82)) {
-      sendMessage = true;
-    }
-  } else if (keyboard_shortcut === "windows") {
-    if (event.ctrlKey && event.altKey && (event.keyCode == 82)) {
-      sendMessage = true;
-    }
-  }
-  
-  if (sendMessage) {
-     safari.self.tab.dispatchMessage("readlater");
-     event.stopPropagation();
-     event.preventDefault();
-     return true;
-  }
-}, false);
-
-safari.self.addEventListener("message", messageHandler, false);
-safari.self.tab.dispatchMessage("getSettings", "keyboard_shortcut");
